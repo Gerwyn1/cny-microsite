f5776d144a438c5ab84ad61b0cb8d26627ffc627
Gerwyn <gerwynsim@hotmail.com>
2023-10-10 17:01:31 +0800

    add more breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -35,42 +35,74 @@ function App() {
   if (!lionDance && !lionScroll) {
     console.log("start");
     return (
-      <div className="relative">
+      <div className="relative m-4">
         <img
           src="/images/lion-cny.webp"
           alt="lion cny"
-          className="xs:h-[80vh] object-cover"
+          className="common:h-[90vh] object-cover"
         />
         {/* cruise, text & button container */}
-        <div className="flex flex-col gap-8 absolute top-[50%] left-[18%] xs:w-2/3 w-full">
+        <div className="flex flex-col absolute top-[50%] 
+        left-[18%] right-[18%]
+        sm:left-[15%] sm:right-[15%]
+        4xl:left-[13%] 4xl:right-[13%]
+
+        gap-8
+        button2:gap-10
+        sm:gap-14
+        common:gap-8
+        2xl:gap-10
+        3xl:gap-12
+        4xl:gap-16
+        6xl:gap-24
+        ">
+          {/* cruise image */}
           <div className=" relative">
             <img className='w-[75%] mx-auto' src="/images/rc_logo.png" alt="cruise" />
           </div>
 
           {/* text & button container */}
-          <div className="flex flex-col gap-1 button1:gap-2">
+          <div className="flex flex-col gap-2 button1:gap-2 button5:gap-3 4xl:gap-5 5xl:gap-7">
+            {/* text */}
             <p
-              className="text-white text-center  
-              
-               mx-auto
-                text-base leading-[1.3rem]
-                button0:text-lg button0:leading-[1.5rem]
-                button1:text-xl button1:leading-[1.6rem]
-                button1.1:text-2xl button1.1:leading-[1.8rem]
-                xs:text-xl xs:leading-[1.5rem]
+              className="text-white text-center mx-auto
+                text-xl leading-[1.3rem] 
+                button0:text-2xl button0:leading-[1.5rem]
+                button1:leading-[1.6rem] 
+                button1.1:leading-[1.8rem]
+                xs:text-3xl
+                button3.1:leading-[2.4rem]
+                button4:text-4xl
+                button5:leading-[2.9rem]
+                sm:leading-[3.2rem]
+                sm:text-5xl
+                common:text-3xl
+                2xl:text-4xl
+                3xl:leading-[2.9rem]
+                4xl:text-5xl
+                5xl:text-6xl
+                6xl:text-8xl
             "
             >
               Are you ready for an abundant new year filled with good luck?
             </p>
+            {/* button */}
             <button
               onClick={() => setLionDance(true)}
               className="shadow-xl rounded-full text-bold  text-white bg-[#0171B8] focus:outline-0 mx-auto
           text-base
-          button1:text-lg
+          py-0 xs:py-1 5xl:py-2 6xl:py-4
+          px-6 xs:px-8 5xl:px-12 6xl:px-20
+          button0:text-xl
           button1.1:text-xl
-          xs:text-lg
-          py-0
-          px-6
+          button2:text-2xl
+          button4:text-3xl
+          button5:text-5xl
+          common:text-2xl
+          2xl:text-3xl
+          3xl:text-4xl
+          5xl:text-5xl
+          6xl:text-7xl
             "
             >
               HUAT AH
