725880c6c81f0062c04c6b1f3da9098a92730e6e
Gerwyn <gerwynsim@hotmail.com>
2023-10-13 18:03:01 +0800

    double pixel density of scroll gif and fix number positioning

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,12 @@ function App() {
   const [lionDance, setLionDance] = useState(false);
   const [cloud, setCloud] = useState(false);
   const [lionScroll, setLionScroll] = useState(false);
+  const [number, setNumber] = useState([]);
 
   console.log("lion dance: ", lionDance);
   console.log("cloud: ", cloud);
   console.log("lion scroll: ", lionScroll);
+  console.log("number: ", number);
 
   let { width, height } = useWindowSize();
 
@@ -31,11 +33,18 @@ function App() {
   useEffect(() => {
     if (isInitialRender.current) {
       document.body.style.background = cloud
-        ? "url('/animation/clouds.gif') center/contain no-repeat #FFE5BF"
+        ? "url('animation/clouds.gif') center/contain no-repeat #FFE5BF"
         : "#FFE5BF";
-      // after 5 sec, roll down scroll with number
       if (lionDance) {
-        const timeoutId = setTimeout(() => setLionScroll(true), 5000);
+        const timeoutId = setTimeout(() => {
+          setNumber([
+            randomDigitImage(),
+            randomDigitImage(),
+            randomDigitImage(),
+            randomDigitImage(),
+          ]);
+          setLionScroll(true);
+        }, 5000);
         return () => clearInterval(timeoutId);
       }
     }
@@ -50,8 +59,8 @@ function App() {
       <>
         <div className="m-4 relative">
           <img
-            className="h-[80vh] object-cover"
-            src="images/full-with-text.webp"
+            className="h-[90vh] object-cover"
+            src="images/start_lion.png"
             alt="lion cny"
           />
           <img
@@ -71,105 +80,7 @@ function App() {
     );
   }
 
-  // END (lion scroll with a random number)
-  //   if (lionScroll) {
-  //     console.log("end screen");
-
-  //     // render gif scroll (gif)
-  //     return (
-  //       <div className="flex justify-center items-center">
-  //         {/* lion-end-scroll */}
-  //         <div className="relative">
-  //           <LazyLoadImage
-  //             className="absolute top-[4%] left-[30%] w-[38%] object-contain"
-  //             src="images/rc_logo.png"
-  //             alt="royal caribbean"
-  //           />
-  //           <LazyLoadImage
-  //             className="absolute top-[39.5%] left-[30%] w-[40%] z-[999] object-contain
-  //             animate-easeInImg
-  //             "
-  //             src="images/title.png"
-  //             alt="title"
-  //           />
-  //           <LazyLoadImage
-  //             className="absolute top-[50.5%] left-[21.5%] w-[57%] z-[999]
-  //             object-contain
-  //             animate-easeInImg
-  //             "
-  //             src="images/number_container.svg"
-  //             alt="number container"
-  //           />
-  //           <LazyLoadImage
-  //             className="absolute top-[82.5%] left-[23.5%] w-[25%] z-[999] object-contain"
-  //             src="images/btn_save.svg"
-  //             alt="save"
-  //           />
-  //           <LazyLoadImage
-  //             onClick={() => {
-  //               setLionDance(false);
-  //               setCloud(false);
-  //               setLionScroll(false);
-  //             }}
-  //             className="absolute top-[82.5%] left-[51.5%] w-[25%] z-[999] object-contain"
-  //             src="images/btn_again.svg"
-  //             alt="try again"
-  //           />
-  //           <LazyLoadImage
-  //             className="absolute top-[88.5%] left-[15%] w-[70%] z-[999] object-contain"
-  //             src="images/instructions.svg"
-  //             alt="instructions"
-  //           />
-  //           <img
-  //             className="h-[80vh] object-cover relative"
-  //             src="/animation/lion-scroll-loop-once-2.gif"
-  //             alt="lion scroll"
-  //           />
-  //           {/* digit images */}
-  //           {/* <div className="absolute">
-
-  // </div> */}
-  //           ``
-  //           <LazyLoadImage
-  //             className="object-cover absolute h-[10%] z-[999]
-  //             top-[57.5%] left-[25%] object-center
-  //             animate-easeInImg
-  //             "
-  //             src={randomDigitImage()}
-  //             alt="random digit"
-  //           />
-  //           <LazyLoadImage
-  //             className="object-cover absolute h-[10%] z-[999]
-  //             top-[57.5%] left-[40%] object-center
-  //             animate-easeInImg
-  //             "
-  //             src={randomDigitImage()}
-  //             alt="random digit"
-  //           />
-  //           <LazyLoadImage
-  //             className="object-cover absolute h-[10%] z-[999]
-  //             top-[57.5%] left-[55%] object-center
-  //             animate-easeInImg
-  //             "
-  //             src={randomDigitImage()}
-  //             alt="random digit"
-  //           />
-  //           <LazyLoadImage
-  //             className="object-cover absolute h-[10%] z-[999]
-  //             top-[57.5%] left-[70%] object-center
-  //             animate-easeInImg
-  //             "
-  //             src={randomDigitImage()}
-  //             alt="random digit"
-  //           />
-  //         </div>
-  //       </div>
-  //     );
-  //   }
-
-  // MIDDLE (dancing lion)
   if (lionDance) {
-    console.log("mid screen");
     return (
       <>
         <div className="absolute left-0 top-0 right-0 bottom-0">
@@ -230,6 +141,7 @@ function App() {
                   setLionDance(false);
                   setCloud(false);
                   setLionScroll(false);
+                  setNumber([]);
                 }}
                 className="absolute top-[82.5%] left-[51.5%] w-[25%] z-[999] object-contain cursor-pointer"
                 src="images/btn_again.svg"
@@ -241,41 +153,41 @@ function App() {
                 alt="instructions"
               />
               <LazyLoadImage
-                className="h-[80vh] object-cover relative"
-                src="/animation/lion-scroll-loop-once-2.gif"
+                className="h-screen object-cover relative"
+                src="animation/end_lion_noloop.gif"
                 alt="lion scroll"
               />
               {/* digit images */}
               <LazyLoadImage
-                className="object-cover absolute h-[10%] z-[999]
+                className="object-cover absolute h-[8%] z-[999]
               top-[57.5%] left-[25%] object-center
               animate-easeInImg
               "
-                src={randomDigitImage()}
+                src={number[0]}
                 alt="random digit"
               />
               <LazyLoadImage
-                className="object-cover absolute h-[10%] z-[999]
-              top-[57.5%] left-[40%] object-center
+                className="object-cover absolute h-[8%] z-[999]
+              top-[57.5%] left-[38%] object-center
               animate-easeInImg
               "
-                src={randomDigitImage()}
+                src={number[1]}
                 alt="random digit"
               />
               <LazyLoadImage
-                className="object-cover absolute h-[10%] z-[999]
-              top-[57.5%] left-[55%] object-center
+                className="object-cover absolute h-[8%] z-[999]
+              top-[57.5%] left-[51%] object-center
               animate-easeInImg
               "
-                src={randomDigitImage()}
+                src={number[2]}
                 alt="random digit"
               />
               <LazyLoadImage
-                className="object-cover absolute h-[10%] z-[999]
-              top-[57.5%] left-[70%] object-center
+                className="object-cover absolute h-[8%] z-[999]
+              top-[57.5%] left-[65%] object-center
               animate-easeInImg
               "
-                src={randomDigitImage()}
+                src={number[3]}
                 alt="random digit"
               />
             </div>
@@ -283,7 +195,7 @@ function App() {
         ) : (
           <LazyLoadImage
             className="h-[80vh] object-contain"
-            src="/animation/animation.gif"
+            src="animation/animation.gif"
             alt="lion dance"
           />
         )}
